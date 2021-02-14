0dc6738cb68e45e473970527e1b214e651a86d29
Muhammad Luqman <g00353385@gmit.ie>
2021-02-15 02:36:59 +0500

    explaining previous code and unsigned int

--- bitoperations.c
+++ bitoperations.c
@@ -1,30 +1,44 @@
 #include <stdio.h>
 
-void bin_print(int i) {
+void bin_print(unsigned int i)
+{
     // number of bits in an integer.
-    int j = (sizeof(int) * 8) -1;
+    int j = (sizeof(unsigned int) * 8) - 1;
 
     // Temporary variable
     int k;
-
-    for (j--; j>=0; j--) {
+// loop over the number of bits in i
+    for (j--; j >= 0; j--)
+    {
+        //
         k = ((1 << j) & i) ? 1 : 0;
-        printf("%d",k);
+        // print k
+        printf("%d", k);
     }
 }
+// standard main signature
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv[]) {
+    // set i to a literal value
+  unsigned int i = 0xffffffff; // 0xf1; // 241 // 4294967295
 
-int i = 241;
-printf("Original: ");
-bin_print(i);
-printf("\n");
+    // what we are printing
+    printf("Original: ");
+    // print i in binary
+    bin_print(i);
+    // End line
+    printf("\t%x\t%d\n",i,i);
 
-for (int j = 0; j < 40; j++) {
-    printf("%3d << %2d: ",i,j );
-    bin_print(i << j);
-    printf("\n");
-}
-
+    for (int j = 0; j < 40; j++)
+    {
+        // what the operation is
+        printf("%3u << %2d: ", i, j);
+        // i shifted left j times
+        bin_print(i << j);
+        // End line
+        printf("\n");
+    }
+    // Everything is ok
     return 0;
 }
